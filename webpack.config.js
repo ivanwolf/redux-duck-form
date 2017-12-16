59ed4c16745f769d6709932b4d843d41469da386
Ivan Wolf <ivanwolf15@gmail.com>
2017-12-16 20:13:22 -0300

    webpack build

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['latest'],
+          },
+        },
+      },
+    ],
+  },
+};
