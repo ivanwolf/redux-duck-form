3379df1d66924c45c8438f60619a561c5ef5f445
Ivan Wolf <ivanwolf15@gmail.com>
2017-12-16 20:21:09 -0300

    preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['latest'],
+            presets: ['env'],
           },
         },
       },
